acecbc799b00b9191de99f660f35fad39da80b51
vinty <vinty1978@gmail.com>
2017-07-13 19:17:47 +0300

    Почти... отображается в логе, но теперь не отображается в браузере.

--- service/src/main/java/by/ecp/aspects/Logger.java
+++ service/src/main/java/by/ecp/aspects/Logger.java
@@ -5,22 +5,26 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-
-
+import org.springframework.stereotype.Component;
 
 
 /**
  * Created by PloSkiY on 09.07.2017.
  */
 @Aspect
+@Component
 public class Logger {
 
     @Around("execution(* *(..)) && @annotation(by.ecp.services.Loggable)")
-    public Object methodLog(ProceedingJoinPoint point)throws Throwable{
+    public void methodLog(ProceedingJoinPoint proceedingJoinPoint)throws Throwable{
         System.out.println("====================== Around method: =========================");
+        System.out.println("Beginning execution for "+proceedingJoinPoint.getSignature().getName());
         long start = System.currentTimeMillis();
-        Object result = point.proceed();
-        return result;
+        proceedingJoinPoint.proceed();
+        long end = System.currentTimeMillis();
+        long res = end-start;
+        System.out.println("Execution completed for "+proceedingJoinPoint.getSignature().getName());
+        System.out.println("Время выполнения: " + res);
     }
 
     @Before("@annotation(by.ecp.services.Loggable)")
